a422fa21b35a61bb38d2c579e1777dabdff7788b
Ambiente 105 <Ambiente 105@GARAPRCADFSD088.FormacionSena.RED>
2024-10-07 20:16:18 -0500

    Organizacion estructura de codigo

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 // Conexión a la base de datos
-$host = "localhost"; // Servidor de MySQL
-$usuarioBD = "root"; // Usuario de MySQL (por defecto en XAMPP es 'root')
-$contraseñaBD = ""; // Contraseña de MySQL (por defecto en XAMPP es vacío)
-$nombreBD = "login"; // Nombre de la base de datos que creaste
+$host = "localhost"; 
+$usuarioBD = "root"; 
+$contraseñaBD = ""; 
+$nombreBD = "login"; 
 
 // Crear conexión
 $conexion = new mysqli($host, $usuarioBD, $contraseñaBD, $nombreBD);
@@ -24,39 +24,44 @@ $stmt->bind_param("ss", $username, $password);
 $stmt->execute();
 $resultado = $stmt->get_result();
 
+// Si las credenciales son correctas
 if ($resultado->num_rows > 0) {
-    // Si las credenciales son correctas
     echo "Login exitoso. ¡Bienvenido!";
-} else {
-    // Si el login falla
+} 
+
+// Si el login falla
+else {
     echo "Usuario o contraseña incorrectos.";
 }
 
 // Crear conexión
 if ($_POST['accion'] == 'registrar_usuario') {
-    // Lógica para guardar los datos del usuario en la base de datos
-    $nombre = $_POST['nombre'];
-    $numeroCel = $_POST['numero_cel'];
-    $id = $_POST['id'];
-    $cumpleanos = $_POST['cumpleanos'];
 
-    // Inserta los datos en la tabla
-    $query = "INSERT INTO datos_usuarios (nombre, numero_cel, id, cumpleanos) VALUES (?, ?, ?, ?)";
-    $stmt = $conexion->prepare($query);
-    $stmt->bind_param("ssiss", $nombre, $numeroCel, $id, $cumpleanos);
+// Lógica para guardar los datos del usuario en la base de datos
+$nombre = $_POST['nombre'];
+$numeroCel = $_POST['numero_cel'];
+$id = $_POST['id'];
+$cumpleanos = $_POST['cumpleanos'];
 
-    if ($stmt->execute()) {
-        echo "Usuario registrado con éxito";
-    } else {
-        echo "Error al registrar usuario: " . $stmt->error;
-    }
+// Inserta los datos en la tabla
+$query = "INSERT INTO datos_usuarios (nombre, numero_cel, id, cumpleanos) VALUES (?, ?, ?, ?)";
+$stmt = $conexion->prepare($query);
+$stmt->bind_param("ssiss", $nombre, $numeroCel, $id, $cumpleanos);
 
-    $stmt->close();
-    $conexion->close();
+//si los datos se guardan correctamente
+if ($stmt->execute()) {
+    echo "Usuario registrado con éxito";
+} 
+
+//si los datos no se guardan correctamente
+else {
+    echo "Error al registrar usuario: " . $stmt->error;
+}
+
+//salida
+$stmt->close();
+$conexion->close();
 }
 $stmt->close();
 $conexion->close();
 ?>
-
-
-
